84ab2afa9edecd02b87c9aa0a3a1a2613429c881
shahriarshawon01 <shahriarshawon001@gmail.com>
2022-07-04 15:51:25 +0600

    Route Theory

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,29 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
+});
+// Simple Route | Route Returning String
+Route::get('uri', function () {
+    return 'Hi Route';
+});
+// Route single parameters
+Route::get('post/{post_id}', function ($post) {
+    return "Route single parameters : " . $post;
+});
+// Route multiple parameters
+Route::get('post/{post_id}/comment/{comment_id?}', function ($p_id, $c_id = 10001) {
+    return "Route multiple parameters : " . '</br>' . 'Post ID: ' . $p_id . '</br>' .
+        'Comment ID: ' . $c_id;
+});
+// Route Parameters & Reguler Expression
+Route::get('reguler_expression/{p_para}', function ($para) {
+    return $para;
+})->where('p_para', '[0-9]+');
+// Routes redirect
+Route::redirect('/here', '/there');
+// Fallback Route
+Route::fallback(function () {
+    return "Fallback Route :)";
 });
